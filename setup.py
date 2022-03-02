2f7ef227a3c602903db435d50f28965c91362b0e
Martin Hunt <martin.g.hunt@gmail.com>
2022-03-02 12:09:31 +0000

    Version bump 0.0.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gat",
-    version="0.0.5",
+    version="0.0.6",
     description="gat: github actions testing",
     packages=find_packages(exclude=["tests"]),
     author="Martin Hunt",
