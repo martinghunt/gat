b8d1a8ecb94b327288562b7f941237e4af4597e8
Martin Hunt <martin.g.hunt@gmail.com>
2022-03-02 11:36:37 +0000

    Vesrion bump 0.0.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gat",
-    version="0.0.4",
+    version="0.0.5",
     description="gat: github actions testing",
     packages=find_packages(exclude=["tests"]),
     author="Martin Hunt",
